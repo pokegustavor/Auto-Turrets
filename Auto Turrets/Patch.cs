28ca10d9c67e4666fc27b32fd2228029e4f2ac26
Gustavo Ribeiro <pokegustavo@gmail.com>
2022-01-06 16:13:15 -0300

    Added auto turrets to stargazer

--- Auto Turrets/Patch.cs
+++ Auto Turrets/Patch.cs
@@ -78,5 +78,24 @@ namespace Extra_Auto_Turrets
                 }
             }
         }
+        [HarmonyPatch(typeof(PLStarGazerInfo), "SetupShipStats")]
+        class StarGazer
+        {
+            static void Postfix(PLStarGazerInfo __instance, bool previewStats)
+            {
+                if (!previewStats)
+                {
+                    GameObject turretPoint = Object.Instantiate(__instance.RegularTurretPoints[0].gameObject, new Vector3(__instance.RegularTurretPoints[0].transform.position.x, __instance.RegularTurretPoints[0].transform.position.y - (__instance.RegularTurretPoints[0].transform.up * 0.134f).y, __instance.RegularTurretPoints[0].transform.position.z + (__instance.RegularTurretPoints[0].transform.forward * 1.29f).z), new Quaternion(__instance.RegularTurretPoints[0].rotation.x, __instance.RegularTurretPoints[0].rotation.y, __instance.RegularTurretPoints[0].rotation.z, __instance.RegularTurretPoints[0].rotation.w));
+                    turretPoint.transform.SetParent(__instance.Exterior.transform);
+                    Transform[] newAutoTurret = new Transform[2];
+                    newAutoTurret[0] = turretPoint.transform;
+                    GameObject turretPoint2 = Object.Instantiate(__instance.RegularTurretPoints[1].gameObject, new Vector3(__instance.RegularTurretPoints[1].transform.position.x, __instance.RegularTurretPoints[1].transform.position.y - (__instance.RegularTurretPoints[1].transform.up * 0.134f).y, __instance.RegularTurretPoints[1].transform.position.z + (__instance.RegularTurretPoints[1].transform.forward * 1.29f).z), new Quaternion(__instance.RegularTurretPoints[1].rotation.x, __instance.RegularTurretPoints[1].rotation.y, __instance.RegularTurretPoints[1].rotation.z, __instance.RegularTurretPoints[1].rotation.w));
+                    turretPoint2.transform.SetParent(__instance.Exterior.transform);
+                    newAutoTurret[1] = turretPoint2.transform;
+                    __instance.AutoTurretPoints = newAutoTurret;
+                    __instance.MyStats.SetSlotLimit(ESlotType.E_COMP_AUTO_TURRET, 2);
+                }
+            }
+        }
     }
 }
